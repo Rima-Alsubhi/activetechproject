c2aa7ef006d5c0706ede86fdb8ca4ecc6054716f
shahad1hassan <shahdhalq@gmail.com>
2024-11-13 01:26:12 +0300

    organize the interface and add back button and background for Network bage

--- app/src/main/java/com/example/activetechproject/NetworkActivity.java
+++ app/src/main/java/com/example/activetechproject/NetworkActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NetworkActivity extends AppCompatActivity {
@@ -13,6 +15,16 @@ public class NetworkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network);
+
+        setContentView(R.layout.activity_network);
+        ImageButton backButton1 = findViewById(R.id.back_button);
+        backButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Back to Home page
+            }
+        });
+
 
         Button btnCyber = findViewById(R.id.btn_cyber);
         btnCyber.setOnClickListener(new View.OnClickListener() {
@@ -37,11 +49,15 @@ public class NetworkActivity extends AppCompatActivity {
                 openChatActivity("Software Community");
             }
         });
-    }
+
+        }
 
     private void openChatActivity(String community) {
         Intent intent = new Intent(NetworkActivity.this, ChatActivity.class);
         intent.putExtra("community_name", community);
         startActivity(intent);
+
     }
+
+
 }
